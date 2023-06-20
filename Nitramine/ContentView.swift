b303c62215289bf50203f3cc974d97f16c67c650
Tarball <alyx.ferrari@gmail.com>
2023-06-20 15:41:12 -0400

    Initial Commit

---
+++ Nitramine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nitramine
+//
+//  Created by tarball on 6/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nitramine/NitramineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NitramineApp.swift
+//  Nitramine
+//
+//  Created by tarball on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NitramineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
